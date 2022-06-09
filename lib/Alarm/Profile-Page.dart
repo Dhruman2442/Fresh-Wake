bf81112d4c7ebde7c4e8f0daea61f8e12185a6c0
Ajay Suman <jayramanuj5@gmail.com>
2022-06-09 14:09:22 +0530

    update

--- lib/Alarm/Profile-Page.dart
+++ lib/Alarm/Profile-Page.dart
@@ -2,6 +2,8 @@ import "package:flutter/material.dart";
 import 'package:fresh_wake/Widgets.dart';
 
 class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() => _ProfilePageState();
 }
@@ -10,78 +12,52 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: TextStyle1("Profile", 20, const Color(0xFFFFFFFF),
+            FontWeight.w600, TextAlign.center, FontStyle.normal),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Color(0xFFFFFFFF),
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
       backgroundColor: const Color(0xFF000000),
       body: SafeArea(
         child: SingleChildScrollView(
           physics: const BouncingScrollPhysics(),
           padding: const EdgeInsets.all(10),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-          Row(
-          children: [
-          CircleAvatar(
-          backgroundColor: Colors.white30,
-            child: IconButton(
-              icon: const Icon(
-                Icons.arrow_back_ios,
-                color: Color(0xFFFFFFFF),
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ),
-          TextStyle1(
-            "Profile",
-            20,
-            const Color(0xFFFFFFFF),
-            FontWeight.w600,
-            TextAlign.center,
-            FontStyle.normal,
-          ),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-              color: Colors.white,
-              height: MediaQuery
-                  .of(context)
-                  .size
-                  .width * 0.85,
-
-              alignment: Alignment.bottomCenter,
-              child: Stack(
-                  children: [
-                  ImageWidget("asset/Profile.png", 93, 93, () {}),
-              Container(
-                color: Color(0xFF000000).withOpacity(0.5),
-                height: MediaQuery
-                    .of(context)
-                    .size
-                    .width * 0.85,
-                child: Column(
-                    children: [
-
-                Container(
-                padding: const EdgeInsets.all(10),
-                child: TextStyle1(
-                    "Dhruman", 50, Color(0xFF000000), FontWeight.w600,
-                    TextAlign.center, FontStyle.normal),),
+              Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 40),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xFF520F0F),
+                          borderRadius: BorderRadius.circular(50)),
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
+                    ),
+                  ),
+                  ImageWidget("asset/Profile.png", 100, 100, () {}),
                 ],
-              ),
-            )
-          ],
+              )
+              // Container(child: ,)
+            ],
+          ),
         ),
-      )
-      ],
-    )],
-    ),
-    ),
-    ),
+      ),
     );
-    }
+  }
 }
